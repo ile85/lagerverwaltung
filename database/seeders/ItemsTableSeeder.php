4afd70edc48e6198d181269f3cea5cd2783dbbd7
ile85 <ilco_dimeski@live.co.uk>
2023-11-20 17:02:14 +0100

    final

--- database/seeders/ItemsTableSeeder.php
+++ database/seeders/ItemsTableSeeder.php
@@ -9,11 +9,11 @@ class ItemsTableSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create();
+        $faker = \Faker\Factory::create('de_DE');
 
         foreach (range(1, 50) as $index) {
             Item::create([
-                'name' => $faker->unique()->word,
+                'name' => $faker->name,
                 'description' => $faker->text(150),
                 'category_id' => $faker->numberBetween(1, 5),
                 'location_id' => $faker->numberBetween(1, 10),
